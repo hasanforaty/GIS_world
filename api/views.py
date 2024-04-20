3a45bec1fd27dad2474133eaec8419c7b8658b46
hasan foraty <hasanforaty19@gmail.com>
2024-04-20 10:02:08 +0330

    fix typo of relocations

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from world.serializers import ShapeFileSerializer
+from api.serializers import ShapeFileSerializer
 from .Features import Features, DoseNotExist
 
 
